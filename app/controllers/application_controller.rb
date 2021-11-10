63a501c20a883b87c331a24fde8c81d47c5fa88f
will14lee <will14lee@gmail.com>
2021-11-10 03:57:17 -0500

    Completed Assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,26 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    baked_good= BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    baked_good.to_json
+  end
+
+  patch '/bakeries/:id' do
+    bakeries= Bakery.find_by(id: params[:id])
+    bakeries.update(
+      name: params[:name]
+    )
+    bakeries.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    baked_good= BakedGood.find_by(id: params[:id])
+    baked_good.destroy
+    baked_good.to_json
+  end
 end
